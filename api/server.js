56399d46eed0d848c47e1fa6cef787953febb4ec
brian-ambani <brainambani1@gmail.com>
2024-04-07 00:07:01 +0300

    create update user API route

--- api/server.js
+++ api/server.js
@@ -3,12 +3,15 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoute.js";
 import authRouter from "./routes/authRoute.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB;
